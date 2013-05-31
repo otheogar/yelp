4b67f73b6ef80e1bf22ab4f92a142df90fb4ac1b
otheogar <oana.catu@gmail.com>
2013-05-31 15:16:31 -0700

    modified parser to select only frequent users and busineses

--- src/yelp/Preprocesser.java
+++ src/yelp/Preprocesser.java
@@ -15,44 +15,66 @@ public class Preprocesser {
 	
 	public static void main(String[] args){
 		
-		if(args.length!=1){
-			System.out.println("Usage: Preprocesser filename");
+		if(args.length!=2){
+			System.out.println("Usage: Preprocesser filename rename");
 			return;
 		}
 		String file = args[0];
-		String name = file.substring(Math.max(0,file.lastIndexOf(FileSystems.getDefault().getSeparator())), file.lastIndexOf("_"));
+		String name = args[1];
+		//String name = file.substring(Math.max(0,file.lastIndexOf(FileSystems.getDefault().getSeparator())), file.lastIndexOf("_"));
 		InputStreamReader in = new InputStreamReader(Preprocesser.class.getClassLoader().getResourceAsStream(file));
 		BufferedReader br = new BufferedReader(in);
 		String line;
 		int i=0;
+		int freq=0;
+		int listLengths=0;
 		
 		try{
-			FileWriter train = new FileWriter(name+"_train.txt");
-			FileWriter test = new FileWriter(name+"_test.txt");
+			FileWriter trainAll = new FileWriter(name+"_trainFreqAll.txt"); //put all the ratings of frequent users/businesses in the training set
+			FileWriter trainAllButLast = new FileWriter(name+"_trainFreqAllButLast.txt"); ////put all but the last the ratings of frequent users/businesses in the training set
+			FileWriter testRare = new FileWriter(name+"_testRare.txt"); //test including only ratings of rare users
+			FileWriter testFreq = new FileWriter(name+"_testFreq.txt"); //test including last rating of freq users
 			while((line=br.readLine())!=null){
 				String[] s = line.split("\\t");
 				String[] list = s[1].split(";");
-				if (list.length>1){
-					//put all but last rating in train
-					test.write(s[0]+"\t"+list[list.length-1]+"\n");
+				if (list.length>10){
+					freq++;
+					listLengths+=list.length;
+					System.out.println(list.length);
+					
+					//put all but last rating in trainAllButLAst - put the last one in testFreq
+					testFreq.write(s[0]+"\t"+list[list.length-1]+"\n");
 					StringBuffer train_line_bf = new StringBuffer(s[0]+"\t"+list[0]);
 					for(int j=1;j<list.length-1;j++)
 						train_line_bf.append(";"+list[j]);
 					train_line_bf.append("\n");
-					train.write(train_line_bf.toString());
+					trainAllButLast.write(train_line_bf.toString());
+					
+					//put all in trainAll
+					trainAll.write(line+"\n");
+					
 				}
 				else{
 					i++;
+				
 					//put one in 10 users/businesses with only one rating in test set
+					/*
 					if (i%10==0) 
 						test.write(line+"\n");
 					else
 						train.write(line+"\n");
+						*/
+					//put all of them in testRare
+					testRare.write(line+"\n");
 				}
 				
 			}
-			train.close();
-			test.close();
+			System.out.println("rows: " + freq);
+			System.out.println("Total reviews: " + listLengths);
+			trainAll.close();
+			trainAllButLast.close();
+			testRare.close();
+			testFreq.close();
 		}
 		catch(IOException e){
 			e.printStackTrace();
--- src/yelp/YelpJsonParser.java
+++ src/yelp/YelpJsonParser.java
@@ -10,32 +10,42 @@ import com.google.gson.JsonStreamParser;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.FileWriter;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map.Entry;
 
 public class YelpJsonParser {
 
+	public static String RECORDS_DELIMETER = ",";
+	public static String BCOUNTFILE = "resources/business_reviewCounts.txt";
+	public static String UCOUNTFILE = "resources/users_reviewCounts.txt";
+	public static int cutoffUser=5;
+	public static int cutoffBusiness=5;
+	private static HashMap<String,Integer> bReviews = new HashMap<String,Integer>();
+	private static HashMap<String,Integer> uReviews = new HashMap<String,Integer>();
+	
 	public YelpJsonParser(){
 
 	}
-	
-	private static void reviewRatings(String uidBid){
-		HashMap<String, String> ratings = new HashMap<String, String>();
-		try{
-			
-			InputStream fileStream = YelpJsonParser.class.getClassLoader().getResourceAsStream("yelp_academic_dataset_review.json");
+
+	private static void count(){
+		
+
+			InputStream fileStream = YelpJsonParser.class.getClassLoader().getResourceAsStream("resources/review.json");
 			InputStreamReader reader = new InputStreamReader(fileStream);
 			Gson myGson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
 			JsonStreamParser jsonParser = new JsonStreamParser(reader);
 			JsonElement element;
-			String idRow;
-			String idCol;
+			String idUser;
+			String idBusiness;
 			int i=0;
+
 			synchronized (jsonParser) {  
 				while(jsonParser.hasNext()) {
 					element = jsonParser.next();
@@ -43,21 +53,78 @@ public class YelpJsonParser {
 					if (i%1000==0)
 						System.out.println(i);
 					Review review = myGson.fromJson(element, Review.class);
+					idUser = review.getUser_id();
+					idBusiness = review.getBusiness_id();
+					if(uReviews.containsKey(idUser)){
+						uReviews.put(idUser, uReviews.get(idUser)+1);
+					}
+					else{
+						uReviews.put(idUser, 1);
+					}
+					if(bReviews.containsKey(idBusiness)){
+						bReviews.put(idBusiness, bReviews.get(idBusiness)+1);
+					}
+					else{
+						bReviews.put(idBusiness, 1);
+					}
+				}
+			}
+			
+		
+		
+	}
+	
+	private static void reviewRatings(String uidBid){
+		System.out.println(uReviews.size());
+		System.out.println(bReviews.size());
+		HashMap<String, String> ratings = new HashMap<String, String>();
+		try{
+			
+			InputStream fileStream = YelpJsonParser.class.getClassLoader().getResourceAsStream("resources/review.json");
+			InputStreamReader reader = new InputStreamReader(fileStream);
+			Gson myGson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+			JsonStreamParser jsonParser = new JsonStreamParser(reader);
+			JsonElement element;
+			String idRow;
+			String idCol;
+			int i=0;
+			boolean freq;
+			int nReviews = 0;
+			synchronized (jsonParser) {  
+				while(jsonParser.hasNext()) {
+					element = jsonParser.next();
+					freq=false;
+					i++;
+					if (i%1000==0)
+						System.out.println(i);
+					Review review = myGson.fromJson(element, Review.class);
 					if(uidBid=="user"){
 						idRow = review.getUser_id();
 						idCol = review.getBusiness_id();
+						if(!uReviews.containsKey(idRow)){
+							System.out.println("no reviewcount for user " + idRow);
+						}
+						if (uReviews.get(idRow) >= cutoffUser && bReviews.get(idCol) >= cutoffBusiness) {
+							freq = true;
+						}
 					}
 					else{
 						idRow = review.getBusiness_id();
 						idCol = review.getUser_id();
+						if (uReviews.get(idCol) >= cutoffUser && bReviews.get(idRow) >= cutoffBusiness) {
+							freq = true;
+						}
 					}
 					
-					if(ratings.containsKey(idRow)) {
-						String old = ratings.get(idRow); 
-						ratings.put(idRow, old+";"+idCol+","+review.getStars());
-					}
-					else {
-						ratings.put(idRow, idCol+","+review.getStars());
+					if(freq){
+						nReviews++;
+						if(ratings.containsKey(idRow)) {
+							String old = ratings.get(idRow); 
+							ratings.put(idRow, old+";"+idCol+","+review.getStars());
+						}
+						else {
+							ratings.put(idRow, idCol+","+review.getStars());
+						}
 					}
 					
 				}
@@ -77,6 +144,8 @@ public class YelpJsonParser {
 				writer.write(kv.getKey() + "\t" + kv.getValue()+"\n");
 			}
 			writer.close();
+			System.out.println("nReviews: " + nReviews);
+			System.out.println(uidBid + " : " + ratings.size());
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -87,7 +156,58 @@ public class YelpJsonParser {
 	}
 
 	public static void main(String[] args){
+		
+		/*
+		
+		URL bFile = YelpJsonParser.class.getClassLoader().getResource(BCOUNTFILE);
+		URL uFile = YelpJsonParser.class.getClassLoader().getResource(UCOUNTFILE);
+		BufferedReader bCountIn = null;
+		BufferedReader uCountIn = null;
+		String line=null;
+		//read files with business review counts and user review counts
+		try {
+		      bCountIn = new BufferedReader(new InputStreamReader(new FileInputStream(bFile.toString().replace("file:", ""))));
+		} catch (FileNotFoundException e1) {
+		      e1.printStackTrace();
+		}
 
+		try {
+			while((line=bCountIn.readLine())!=null){
+				String[] entries = line.split(RECORDS_DELIMETER);
+				bReviews.put(entries[0],Integer.parseInt(entries[1]));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+		      uCountIn = new BufferedReader(new InputStreamReader(new FileInputStream(uFile.toString().replace("file:", ""))));
+		} catch (FileNotFoundException e1) {
+		      e1.printStackTrace();
+		}
+
+		try {
+			while((line=uCountIn.readLine())!=null){
+				String[] entries = line.split(RECORDS_DELIMETER);
+				uReviews.put(entries[0],Integer.parseInt(entries[1]));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		*/
+		
+		if(args.length!=2){
+			System.out.println("usage: YelpJsonParser cutoffUserNReviews cutoffBusinessNReviews");
+			return;
+		}
+		
+		cutoffUser = Integer.parseInt(args[0]);
+		cutoffBusiness = Integer.parseInt(args[1]);
+		count();
+		System.out.println(uReviews.size());
+		System.out.println(bReviews.size());
+		
 		//read reviews  and output as userid....businesId, rating
 		reviewRatings("user");
 		reviewRatings("business");
